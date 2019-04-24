3527232fde1bd67053a24e00c87f688687786f6c
Juan José Sosa Osuna <sosaosuna@gmail.com>
2019-04-24 13:08:11 +0200

    Added auth routes file to app

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
 const app = express();
 
-// setup view engine
+// Set up view engine
 app.set('view engine', 'ejs');
+
+// Set up routes
+app.use('/auth', authRoutes);
 
 // Create home route
 app.get('/', (req, res) => {
