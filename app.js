e62d7bb3c93985aa87eee52adc8412b2cd866721
Juan José Sosa Osuna <sosaosuna@gmail.com>
2019-04-25 09:26:17 +0200

    Connection to mongodb

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
 
 // Set up view engine
 app.set('view engine', 'ejs');
+
+// Connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+    console.log('Connection with MongoDB established');
+});
 
 // Set up routes
 app.use('/auth', authRoutes);
